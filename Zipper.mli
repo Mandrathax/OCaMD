fce4583c5eb2da470dbe47f7086d973e7ac30b43
Mandrathax <pmichel31415@gmail.Com>
2015-06-03 00:05:09 +0200

    error correction

--- Zipper.mli
+++
@@ -1,7 +0,0 @@
-val encode : Huffman.encoding_dictionary -> Huffman.plain_text -> Huffman.cipher_text
-
-val decode : Huffman.decoding_dictionary -> Huffman.cipher_text -> Huffman.plain_text
-
-val compress : string -> unit
-
-val decompress : string -> unit
